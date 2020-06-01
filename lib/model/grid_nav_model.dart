0ae2ec2d67948caca1d9418ae286e51f034bfcbd
wanghao <1750110604@qq.com>
2020-06-01 16:04:38 +0800

    model

---
+++ lib/model/grid_nav_model.dart
@@ -0,0 +1,59 @@
+// hotel  GridNavItem  NonNull
+// flight  GridNavItem  NonNull
+// travel  GridNavItem  NonNull
+import 'package:flutter_trip/model/common_model.dart';
+
+class GridNavModel {
+  final GridNavItem hotel;
+  final GridNavItem flight;
+  final GridNavItem travel;
+
+  GridNavModel({this.hotel, this.flight, this.travel});
+
+  factory GridNavModel.fromJson(Map<String, dynamic> json) {
+    return json != null
+        ? GridNavModel(
+            hotel: GridNavItem.fromJson(json['hotel']),
+            flight: GridNavItem.fromJson(json['flight']),
+            travel: GridNavItem.fromJson(json['travel']))
+        : null;
+  }
+}
+
+// startColor  String  NonNull
+// endColor  String  NonNull
+// mainItem  CommonModel  NonNull
+// item1  CommonModel  NonNull
+// item2  CommonModel  NonNull
+// item3  CommonModel  NonNull
+// item4  CommonModel  NonNull
+class GridNavItem {
+  final String startColor;
+  final String endColor;
+  final CommonModel mainItem;
+  final CommonModel item1;
+  final CommonModel item2;
+  final CommonModel item3;
+  final CommonModel item4;
+
+  GridNavItem(
+      {this.startColor,
+      this.endColor,
+      this.mainItem,
+      this.item1,
+      this.item2,
+      this.item3,
+      this.item4});
+
+  factory GridNavItem.fromJson(Map<String, dynamic> json) {
+    return GridNavItem(
+      startColor: json['startColor'],
+      endColor: json['endColor'],
+      mainItem: CommonModel.fromJson(json['mainItem']),
+      item1: CommonModel.fromJson(json['item1']),
+      item2: CommonModel.fromJson(json['item2']),
+      item3: CommonModel.fromJson(json['item3']),
+      item4: CommonModel.fromJson(json['item4']),
+    );
+  }
+}
